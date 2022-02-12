383a3182f24664bc2cef118acfa74c70bc3c6013
sym12312 <56857191+sym12312@users.noreply.github.com>
2022-02-12 21:28:49 +0800

    Update GUI_pygame.py

--- GUI_tests/GUI_pygame.py
+++ GUI_tests/GUI_pygame.py
@@ -19,20 +19,6 @@ set icon below
 # icon = pygame.image.load('')
 # pygame.display.set_icon(icon)
 
-
-
-def rot_center(image, angle):
-    """rotate an image while keeping its center and size"""
-    orig_rect = image.get_rect()
-    rot_image = pygame.transform.rotate(image, angle)
-    rot_rect = orig_rect.copy()
-    rot_rect.center = rot_image.get_rect().center
-    rot_image = rot_image.subsurface(rot_rect).copy()
-    return rot_image
-
-
-
-
 class Car(pygame.sprite.Sprite):
     def __init__ (self, pos, picture_path):
         super().__init__()
